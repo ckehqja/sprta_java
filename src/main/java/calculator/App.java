8ce15e6c591b3f721a8f4e13fb06e458590c8036
ckehqja <ckehqja@gmail.com>
2024-04-22 20:21:15 +0900

    2. Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다. + 매개변수로 Scanner를 받아 중복문제 해결

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,22 +4,42 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int a = typeCheck();
-            System.out.print("두 번째 숫자를 입력하세요: ");
+        int a = typeCheck(sc);
+        System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int b = typeCheck();
+        int b = typeCheck(sc);
+
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operation = operationCheck(sc);
 
         //사용자에게 입력받은 값을 출력
         System.out.println("a = " + a);
         System.out.println("b = " + b);
+        System.out.println("operation = " + operation);
+
 
     }
 
+    private static char operationCheck(Scanner sc) {
+        boolean isOperation = false;
+        char operations;
+        do {
+            System.out.println("사칙연산 중 하나를 입력해주세요(-, +, *, /");
+            operations = sc.next().charAt(0);
+            if(operations == '+' || operations == '-' || operations == '*' || operations == '/') {
+                isOperation = true;
+            }
+        } while (!isOperation);
+        return operations;
+    }
+
     //사용자에게 0이상의 정수를 받는 메서드(음수일때 반복해서 값을 요청)
-    static int typeCheck() {
-        Scanner sc = new Scanner(System.in);
+    static int typeCheck(Scanner sc) {
         int a;
         do {
             a = sc.nextInt();
